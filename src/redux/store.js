80b4146072de5730a9900ddb85543fc7b424cf2c
mrcbq <marcobaquero@gmail.com>
2023-06-13 10:32:19 -0500

    Change export to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducers: { books: booksReducer, categories: categoriesReducer },
 });
 
