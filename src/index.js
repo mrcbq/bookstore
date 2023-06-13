5622fa860b94f5ee64d0ca9110d3b6743cc070fa
mrcbq <marcobaquero@gmail.com>
2023-06-13 10:02:08 -0500

    Add Provider to index.js and organized the file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { Provider } from 'react-redux';
 import store from './redux/store';
+
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
