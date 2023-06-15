0d4f1cd72c749931ef85c55ff5ce24c37ce82590
mrcbq <marcobaquero@gmail.com>
2023-06-14 20:13:29 -0500

    End styles for NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,32 @@
 import { NavLink } from 'react-router-dom';
+
+import 'material-icons/iconfont/material-icons.css';
 import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div id="navbarContainer">
-      <span className="Bookstore-CMS">Bookstore CMS</span>
-      <ul>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'active' : '')}
-            to="/"
-          >
-            BOOKS
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'active' : '')}
-            to="/categories"
-          >
-            CATEGORIES
-          </NavLink>
-        </li>
-      </ul>
+      <div className="nav">
+        <span className="Bookstore-CMS">Bookstore CMS</span>
+        <ul>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              to="/"
+            >
+              BOOKS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              to="/categories"
+            >
+              CATEGORIES
+            </NavLink>
+          </li>
+        </ul>
+      </div>
       <button className="icon-button" type="button">
         <span className="material-icons primary-color">person</span>
       </button>
